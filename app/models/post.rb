857d35ede5e417b45cbccd740bda02a20e7a03ed
kmSidharthM <kmsidharthmurali@gmail.com>
2024-08-16 13:37:19 +0530

    Added validations for title, net votes and is_blog_worthy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
+  MAXIMUM_TITLE_LENGTH = 125
 
+  validates :title, presence: true, length: {maximum: MAXIMUM_TITLE_LENGTH}
+  validate :is_net_vote_negative
+  validates_inclusion_of :is_blog_worthy, in: [true, false]
+
+  def is_net_vote_negative
+    if upvotes - downvotes < 0
+      errors.add(:base, "Net votes cannot acquire a negative value")
+    end
+  end
 end
