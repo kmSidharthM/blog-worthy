3ecd62192b70999979f45846b7251686e56484b3
kmSidharthM <kmsidharthmurali@gmail.com>
2024-09-05 20:11:47 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   MAXIMUM_TITLE_LENGTH = 125
 
-  validates :title, presence: true, length: {maximum: MAXIMUM_TITLE_LENGTH}
+  validates :title, presence: true, length: { maximum: MAXIMUM_TITLE_LENGTH }
   validate :is_net_vote_negative
   validates_inclusion_of :is_blog_worthy, in: [true, false]
 
